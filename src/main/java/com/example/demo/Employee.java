873fa84f7f70509888046dbb033c3d578c5ca524
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-26 15:30:51 -0500

    first commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String jobTitle;
+    private String name;
+    private String age;
+
+    @ManyToOne//(fetch = FetchType.EAGER)
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+}
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Autowired
+    CompanyRepository companyRepository;
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("employees", employeeRepository.findAll());
+        model.addAttribute("companies", companyRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/addCompany")
+    public String addCompany(Model model) {
+        model.addAttribute("company", new Company());
+        return "companyform";
+    }
+
+    @PostMapping("/processCompany")
+    public String processCompany(@ModelAttribute Company company) {
+        companyRepository.save(company);
+        return "redirect:/";
+    }
+
+    @GetMapping("/addEmployee")
+    public String addEmployee(Model model) {
+        model.addAttribute("companies", companyRepository.findAll());
+        model.addAttribute("employee", new Employee());
+        return "companyform";
+    }
+
+    @PostMapping("/processEmployee")
+    public String processEmployee(@ModelAttribute Employee employee) {
+        employeeRepository.save(employee);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showEmployee(@PathVariable("id") long id, Model model) {
+        model.addAttribute("employee", employeeRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateEmployee(@PathVariable("id") long id, Model model) {
+        model.addAttribute("employee", employeeRepository.findById(id).get());
+        return "employeeform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String udelEmployee(@PathVariable("id") long id) {
+        employeeRepository.deleteById(id);
+        return "redirect:/";
+    }
+}
